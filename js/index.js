0ea158df28fa56a216fb66cf1876bf69679c73ed
apamanger <rfoley00@live.com>
2023-05-24 22:46:43 -0400

    added timers and counter for clicks

--- js/index.js
+++ js/index.js
@@ -20,16 +20,42 @@ usernameForm.addEventListener("submit", function(event) {
   modal.style.display = "none";
 });
 //end modal
+var clickedSquares = new Set();
+
 function changeColor(event) {
   const square = event.target;
+
+  // Check if the square has already been clicked
+  if (!clickedSquares.has(square)) {
+    clickedSquares.add(square); // Add the square to clickedSquares set
+  }
+
   const colors = ['color1', 'color2', 'color3', 'color4', 'color5'];
   const currentColorIndex = square.dataset.colorIndex || 0;
   const nextColorIndex = (parseInt(currentColorIndex) + 1) % colors.length;
 
   square.className = 'square ' + colors[nextColorIndex];
   square.dataset.colorIndex = nextColorIndex;
+
+  const count = 20 - clickedSquares.size;
+  document.getElementById('counter').textContent = 'Count: ' + count;
+
+  if (count <= 0) {
+    // Perform necessary actions when count reaches 20
+    document.getElementById('counter').textContent = 'Out of Moves';
+    disableColorChanges();
+  }
+  console.log(clickedSquares)
+}
+// once all 20 moves are made this function stops color changes
+function disableColorChanges() {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach((square) => {
+    square.removeEventListener('click', changeColor);
+  });
 }
 
+//creates divs for the grid
 function createGrid() {
   const grid = document.getElementById('grid');
 
@@ -40,6 +66,27 @@ function createGrid() {
     square.addEventListener('click', changeColor);
     grid.appendChild(square);
   }
+  startCountdownTimer();
+}
+// 5 minute timer countdown
+function startCountdownTimer() {
+  var countDownDate = new Date().getTime() + (5 * 60 * 1000); // Set countdown to 5 minutes (300 seconds)
+
+  var countdownClock = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+
+
+    if (distance < 0) {
+      clearInterval(countdownClock);
+      document.getElementById("timer").innerHTML = "Time Expired";
+    }
+  }, 1000);
 }
 
 // Call the function to create the grid
